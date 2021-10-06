7a92c22d6b9a1a34c2d731f033ddc170cba467e0
malolebrin <malolebrin@gmail.com>
2021-10-06 11:54:39 +0200

    COnfig package json

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,6 @@
   </div>
 </template>
 
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
