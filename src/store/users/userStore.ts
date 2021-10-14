ef414a460bfe772182061f975051cb862dbe70a7
malolebrin <malolebrin@gmail.com>
2021-10-14 17:33:49 +0200

    FEAT create user getters actions

--- src/store/users/userStore.ts
+++ src/store/users/userStore.ts
@@ -1,29 +1,18 @@
 import { EntitiesEnum } from '../../types/globals'
 import { defineStore } from "pinia"
-import { UserState, UserType } from './types'
+import { UserType } from './types'
 import { userState } from './state'
+import createGetters from '../utils/createGetters'
+import createActions from '../utils/createActions'
 
 export const useUserStore = defineStore(EntitiesEnum.USERS, {
 	state: () => ({
 		...userState
 	}),
 	getters: {
-		// getCurrentUserFullName: state => `${state.currentUser.firstName} ${state.currentUser.lastName}`,
-		// findOneById: (state) => (userId: number) => state.users.find(user => user.id === userId),
+		...createGetters<UserType>(userState),
 	},
 	actions: {
-		// TODO alert errors state api in pinia
-		// addUser(user: UserType) {
-		// 	if (!this.users.find(u => u.id === user.id)) {
-		// 		this.users.push(user)
-		// 		return this.users
-		// 	}
-		// },
-		// setCurrentUser(user: UserType) {
-		// 	this.currentUser = user
-		// },
-		// removeCurrentUser() {
-		// 	this.currentUser = {} as UserType
-		// }
+		...createActions<UserType>(userState),
 	},
 })
