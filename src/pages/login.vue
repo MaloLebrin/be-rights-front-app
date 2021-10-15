cf8265ac39c1726f95d66ba21074ae40cb459f7b
malolebrin <malolebrin@gmail.com>
2021-10-15 12:10:51 +0200

    FEAT start login page

---
+++ src/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Bienvenue sur Be right</h1>
+
+  <BCardModal is-active>
+    <BField label="Address e-mail">
+      <BInput />
+    </BField>
+    <BField label="Mot de passe">
+      <BInput />
+    </BField>
+  </BCardModal>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  name: 'Login',
+  setup() {
+    const form = reactive({
+      email: '',
+      password: '',
+    })
+
+  },
+})
+</script>
