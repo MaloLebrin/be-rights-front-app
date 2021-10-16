516dfbd680ef40902106a2bfd466423ee24f58b7
malolebrin <malolebrin@gmail.com>
2021-10-16 17:36:31 +0200

    init routes prepare to layout

--- src/types/shims/components.d.ts
+++ src/types/shims/components.d.ts
@@ -4,12 +4,15 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    404: typeof import('./../../pages/404.vue')['default']
+    AdminDashboard: typeof import('./../../pages/adminDashboard/index.vue')['default']
     Header: typeof import('./../../components/header/Header.vue')['default']
     Index: typeof import('./../../pages/Index.vue')['default']
     Login: typeof import('./../../pages/login.vue')['default']
     LoginModal: typeof import('./../../components/modals/LoginModal.vue')['default']
     MenuDrawer: typeof import('./../../components/drawer/MenuDrawer.vue')['default']
     SimpleLogo: typeof import('./../../components/logo/SimpleLogo.vue')['default']
+    UserDashboard: typeof import('./../../pages/userDashboard/index.vue')['default']
   }
 }
 
