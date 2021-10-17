accb9747b6493d65bc9e18187e88353caf2db334
malolebrin <malolebrin@gmail.com>
2021-10-17 16:49:48 +0200

    FEAT redirect if user is loggedin with cookies

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,22 @@
   </router-view>
 </template>
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
-import { useMainStore } from './store/mainStore'
+import { computed, defineComponent, watch } from 'vue'
+import { useMainStore } from '@/store/mainStore'
+import userHook from '@/hooks/userHook'
 
 export default defineComponent({
   setup() {
     const store = useMainStore()
-    // const user = {
-    //   id: 1,
-    //   email: 'malolebrin@gmail.com',
-    //   firstName: 'Malo',
-    //   lastName: 'Lebrin',
-    //   companyName: 'kissmy',
-    //   siret: '5735757657',
-    // }
-    // console.log(store.getCurrent, 'store.getCurrent')
+    const { routesIntermsOfUserRoles } = userHook()
+
+    watch(() => store.setIsLoggedIn, () => {
+      routesIntermsOfUserRoles()
+    })
+    routesIntermsOfUserRoles()
+
     const isLoggedIn = computed(() => store.isLoggedIn)
+
     return {
       isLoggedIn,
     }
@@ -40,8 +40,6 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   display: flex;
-  /* align-items: center;
-  justify-content: center; */
   background-color: grey;
 }
 </style>
--- src/hooks/userHook.ts
+++ src/hooks/userHook.ts
@@ -4,11 +4,13 @@ import { UserType } from "@/store/users/types"
 import { useUserStore } from "@/store/users/userStore"
 import { parseEntity } from '~/store/utils/store'
 import { useCookie } from 'vue-cookie-next'
+import router from '~/router'
 
 export default function userHook() {
 	const userStore = useUserStore()
 	const mainStore = useMainStore()
-	const { setCookie, removeCookie } = useCookie()
+	const { setCookie, getCookie } = useCookie()
+
 	async function login({ email, password }: { email: string, password: string }) {
 		try {
 			mainStore.toggleIsLoading()
@@ -31,8 +33,21 @@ export default function userHook() {
 		mainStore.resetAllState()
 	}
 
+	function routesIntermsOfUserRoles() {
+		const token = getCookie('userToken')
+		if (token && token.length > 0) {
+			mainStore.setIsLoggedIn()
+			if (userStore.isCurrentUserAdmin) {
+				router.push('adminDashbord')
+			}
+			router.push('userDashboard')
+		}
+		router.push('login')
+	}
+
 	return {
 		login,
 		logout,
+		routesIntermsOfUserRoles,
 	}
 }
