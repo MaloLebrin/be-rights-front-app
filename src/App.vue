e7034ef2dd61a26dab8ae7b6478c7e92af204aa0
malolebrin <malolebrin@gmail.com>
2021-11-07 17:40:58 +0100

    FEAT display events admin events list

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,11 @@
 import { watch, onBeforeMount } from 'vue'
 import { useMainStore } from '@/store/mainStore'
 import userHook from '@/hooks/userHook'
+import { useEventStore } from './store/events/eventStore'
+import { EventType } from './store/events/types'
     const store = useMainStore()
+    const eventStore = useEventStore()
+    const { createMany } = eventStore
     const { routesIntermsOfUserRoles } = userHook()
 
     watch(() => [store.setIsLoggedIn,], async () => {
@@ -18,6 +22,101 @@ import userHook from '@/hooks/userHook'
     })
 
     onBeforeMount(async () => await routesIntermsOfUserRoles())
+
+    const events =[
+            {
+                documentId: [],
+                employeeList: [],
+                status: "CREATE",
+                creadtedAt: "2021-11-05T13:37:12.030Z",
+                updatedAt: "2021-11-05T13:37:12.030Z",
+                signatureCount: 0,
+                totalSignatureNeeded: 0,
+                id: 1,
+                name: "Event1",
+                start: "2022-09-22T15:00:00.000Z",
+                end: "2023-09-22T15:00:00.000Z",
+                address: "2 rue de france",
+                postalCode: 41000,
+                city: "ville",
+                country: "France",
+                userId: 1,
+            },
+            {
+                documentId: [],
+                employeeList: [],
+                status: "CREATE",
+                creadtedAt: "2021-11-05T13:37:12.030Z",
+                updatedAt: "2021-11-05T13:37:12.030Z",
+                signatureCount: 0,
+                totalSignatureNeeded: 0,
+                id: 2,
+                name: "Event1",
+                start: "2022-09-22T15:00:00.000Z",
+                end: "2023-09-22T15:00:00.000Z",
+                address: "2 rue de france",
+                postalCode: 41000,
+                city: "ville",
+                country: "France",
+                userId: 1,
+            },
+            {
+                documentId: [],
+                employeeList: [],
+                status: "CREATE",
+                creadtedAt: "2021-11-05T13:37:12.030Z",
+                updatedAt: "2021-11-05T13:37:12.030Z",
+                signatureCount: 0,
+                totalSignatureNeeded: 0,
+                id: 3,
+                name: "Event1",
+                start: "2022-09-22T15:00:00.000Z",
+                end: "2023-09-22T15:00:00.000Z",
+                address: "2 rue de france",
+                postalCode: 41000,
+                city: "ville",
+                country: "France",
+                userId: 1,
+            },
+            {
+                documentId: [],
+                employeeList: [],
+                status: "CREATE",
+                creadtedAt: "2021-11-05T13:37:12.030Z",
+                updatedAt: "2021-11-05T13:37:12.030Z",
+                signatureCount: 0,
+                totalSignatureNeeded: 0,
+                id: 4,
+                name: "Event1",
+                start: "2022-09-22T15:00:00.000Z",
+                end: "2023-09-22T15:00:00.000Z",
+                address: "2 rue de france",
+                postalCode: 41000,
+                city: "ville",
+                country: "France",
+                userId: 1,
+            },
+            {
+                documentId: [],
+                employeeList: [],
+                status: "CREATE",
+                creadtedAt: "2021-11-05T13:37:12.030Z",
+                updatedAt: "2021-11-05T13:37:12.030Z",
+                signatureCount: 0,
+                totalSignatureNeeded: 0,
+                id: 5,
+                name: "Event1",
+                start: "2022-09-22T15:00:00.000Z",
+                end: "2023-09-22T15:00:00.000Z",
+                address: "2 rue de france",
+                postalCode: 41000,
+                city: "ville",
+                country: "France",
+                userId: 1,
+            }
+        ]
+    createMany(events as EventType[])
+
 </script>
 
 <style>
--- src/pages/adminDashboard/index.vue
+++ src/pages/adminDashboard/index.vue
@@ -1,8 +1,45 @@
 <template>
-  <div class="text-green dark:bg-blue-dark min-h-screen transform ease-in-out transition-all duration-500">
+  <div class="bg-white-light dark:bg-blue-dark min-h-screen transform ease-in-out transition-all duration-500 py-6 text-left pl-14 pr-8">
+    <div class="grid grid-cols-3 items-center mb-14">
+      <h3 class="text-2xl font-semibold text-gray-800 dark:text-white flex items-center max-w-xs mt">
+        <HomeIcon class="h-8 mr-4 dark:bg-red rounded-lg p-1" />
+        Événements
+      </h3>
+      <div class="flex col-span-2 items-center justify-center">
+        <BButton class="mr-2 dark:text-black">Tout</BButton>
+        <BButton class="mr-2 dark:text-black">En cours</BButton>
+        <BButton class="mr-2 dark:text-black">À venir</BButton>
+        <BButton class="mr-2 dark:text-black">Terminés</BButton>
+        <BInput type="text" placeholder="Recherchez" v-model="search" />
+      </div>
+    </div>
 
-    admin dashboard
-  </div>
+    <BAccordion
+      v-for="event in events"
+      :key="event.id"
+      class="flex flex-col items-center mb-6 bg-white dark:bg-blue-dark_bold rounded-full shadow-2xl
+      hover:-translate-y-2 animate-fade-in-down max-w-3xl pb-4"
+    >
+      <template #title>
+        <div class="flex items-center justify-between px-5 py-2 font-semibold">
+          <div>{{ event.id }}</div>
+          <span class="bg-gray">{{ event.name }}</span>
+          <span>{{ event.start }}</span>
+          <button>relancer</button>
+          <div>
+            {{ event.signatureCount}}/{{ event.totalSignatureNeeded }}
+          </div>
+          <span>{{ getEventStatusTranslation(event.status) }}</span>
+        </div>
+      </template>
+
+      <div class="px-4 mt-2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis quae neque maiores velit itaque nisi, maxime repellat mollitia laudantium distinctio voluptate veritatis natus fugit accusamus iure illo minima molestias inventore.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus laboriosam voluptatum maiores commodi nam, dignissimos a dolore assumenda excepturi repellat rerum non asperiores fugit vitae ea illo facilis neque! Quis!
+      </div>
+    </BAccordion>
+
+      </div>
 </template>
 <route>
 {meta: {
@@ -10,3 +47,16 @@
 }
 }
 </route>
+
+<script setup lang="ts">
+import { HomeIcon } from '@heroicons/vue/outline'
+import { computed, ref } from 'vue'
+import { useEventStore } from '~/store/events/eventStore'
+import eventHook from '~/hooks/eventHook'
+
+const { getEventStatusTranslation } = eventHook()
+const eventStore = useEventStore()
+const search = ref('')
+const events = computed(() => Object.values(eventStore.entities.byId))
+console.log(events.value, 'events.value')
+</script>
