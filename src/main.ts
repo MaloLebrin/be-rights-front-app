99f7cdd6933ab802ee133066fb47f2cb94efaab4
malolebrin <malolebrin@gmail.com>
2021-11-13 15:10:52 +0100

    FIX alias path

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import library from 'be-right-vue3-lib'
 import { createApp } from 'vue'
-import router from '@/router/index'
+import router from '~/router/index'
 import App from './App.vue'
 import './assets/tailwind.css'
 import { createPinia } from 'pinia'
