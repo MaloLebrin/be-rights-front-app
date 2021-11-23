c81b664751f1c6df8910235a4fdb78b86a151aec
malolebrin <malolebrin@gmail.com>
2021-11-23 21:28:03 +0100

    updagte compoennts auto import

--- src/types/shims/components.d.ts
+++ src/types/shims/components.d.ts
@@ -9,8 +9,11 @@ declare module 'vue' {
     AdminMenuDrawer: typeof import('./../../components/drawer/AdminMenuDrawer.vue')['default']
     BDrawer: typeof import('./../../components/drawer/BDrawer.vue')['default']
     DarkModeToggle: typeof import('./../../components/content/DarkModeToggle.vue')['default']
+    EmployeeEventItem: typeof import('./../../components/employees/EmployeeEventItem.vue')['default']
+    EventItem: typeof import('./../../components/events/EventItem.vue')['default']
     Footer: typeof import('./../../components/footer/Footer.vue')['default']
     Header: typeof import('./../../components/header/Header.vue')['default']
+    HeaderList: typeof import('./../../components/header/HeaderList.vue')['default']
     Index: typeof import('./../../pages/Index.vue')['default']
     Login: typeof import('./../../pages/login.vue')['default']
     LoginModal: typeof import('./../../components/modals/LoginModal.vue')['default']
@@ -18,6 +21,7 @@ declare module 'vue' {
     SimpleLogo: typeof import('./../../components/logo/SimpleLogo.vue')['default']
     StepBubble: typeof import('./../../components/content/StepBubble.vue')['default']
     UserDashboard: typeof import('./../../pages/userDashboard/index.vue')['default']
+    Users: typeof import('./../../pages/adminDashboard/users.vue')['default']
   }
 }
 
