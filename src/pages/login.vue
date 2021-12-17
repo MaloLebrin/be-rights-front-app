92c1db790c266b66154992926633ec6b551b5ab4
malolebrin <malolebrin@gmail.com>
2021-12-17 11:28:19 +0100

    rework: login page register page style

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,44 +1,21 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-full w-full
-  bg-white">
-    <BCard
-      variant="white"
-      class="p-16 dark:bg-blue-dark_bold DarkModeAnimation"
-    >
-      <div class="mb-6">
-        <h1 class="text-black dark:text-white">Bienvenue sur</h1>
-        <SimpleLogo />
-      </div>
+  <div class="flex flex-col items-center h-full w-full bg-white">
+    <div class="mb-6">
+      <h1 class="text-black dark:text-white">Bienvenue sur</h1>
+      <SimpleLogo />
+    </div>
 
-      <BField label="Address e-mail">
-        <BInput
-          type="email"
-          class="text-black"
-          v-model="form.email"
-        />
-      </BField>
-      <BField label="Mot de passe">
-        <BInput
-          type="password"
-          class="text-black"
-          v-model="form.password"
-        />
-      </BField>
-      <div class="grid grid-cols-1 gap-4">
-        <BButton
-          :disabled="isSubmitDisabled"
-          variant="danger"
-          @click="submitLogin"
-        >Se Connecter</BButton>
-				<BLink
-					tag="router-link"
-					to="/register"
-				>
-					S'inscrire
-				</BLink>
-        <BLink>Mot de passe oublié</BLink>
-      </div>
-    </BCard>
+    <BField label="Address e-mail">
+      <BInput type="email" class="text-black" v-model="form.email" />
+    </BField>
+    <BField label="Mot de passe">
+      <BInput type="password" class="text-black" v-model="form.password" />
+    </BField>
+    <div class="grid grid-cols-1 gap-4">
+      <BButton :disabled="isSubmitDisabled" variant="danger" @click="submitLogin">Se Connecter</BButton>
+      <BLink tag="router-link" to="/register">S'inscrire</BLink>
+      <BLink>Mot de passe oublié</BLink>
+    </div>
   </div>
 </template>
 
@@ -46,16 +23,16 @@
 import { userHook } from '@/hooks'
 import { computed, reactive } from 'vue'
 
-    const { login } = userHook()
-    const form = reactive({
-      email: '',
-      password: '',
-    })
+const { login } = userHook()
+const form = reactive({
+  email: '',
+  password: '',
+})
 
-    async function submitLogin() {
-      await login({ email: form.email, password: form.password })
-    }
+async function submitLogin() {
+  await login({ email: form.email, password: form.password })
+}
 
-    const isSubmitDisabled = computed(() => form.email.length === 0 || form.password.length === 0)
+const isSubmitDisabled = computed(() => form.email.length === 0 || form.password.length === 0)
 
 </script>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,6 +1,5 @@
 <template>
-	<div class="flex flex-col items-center h-full
-	bg-white">
+	<div class="flex flex-col items-center h-full bg-white">
 		<div class="mb-6 mt-10">
 			<h1 class="text-black dark:text-white">Bienvenue sur</h1>
 			<SimpleLogo />
@@ -19,9 +18,7 @@
 				<label
 					:for="RoleEnum.PHOTOGRAPHER"
 					class="ml-2 block text-sm text-gray-900"
-				>
-					je suis un photographe ou agence de photographie
-				</label>
+				>je suis un photographe ou agence de photographie</label>
 			</div>
 			<div class="flex items-center">
 				<input
@@ -35,75 +32,36 @@
 				<label
 					:for="RoleEnum.COMPANY"
 					class="ml-2 block text-sm text-gray-900"
-				>
-					je suis une enteprise ou un particulier
-				</label>
+				>je suis une enteprise ou un particulier</label>
 			</div>
 
-			<BField
-				class="col-span-2"
-				label="Nom de l'entreprise"
-			>
-				<BInput
-					type="text"
-					class="text-black"
-					v-model="form.companyName"
-				/>
+			<BField class="col-span-2" label="Nom de l'entreprise">
+				<BInput type="text" class="text-black" v-model="form.companyName" />
 			</BField>
 
 			<BField label="Prénom">
-				<BInput
-					type="text"
-					class="text-black"
-					v-model="form.firstName"
-				/>
+				<BInput type="text" class="text-black" v-model="form.firstName" />
 			</BField>
 			<BField label="Nom">
-				<BInput
-					type="text"
-					class="text-black"
-					v-model="form.lastName"
-				/>
+				<BInput type="text" class="text-black" v-model="form.lastName" />
 			</BField>
-			<BField
-				class="col-span-2"
-				label="Address e-mail"
-			>
-				<BInput
-					type="email"
-					class="text-black"
-					v-model="form.email"
-				/>
+			<BField class="col-span-2" label="Address e-mail">
+				<BInput type="email" class="text-black" v-model="form.email" />
 			</BField>
-			<BField
-				class="col-span-2"
-				label="Mot de passe"
-			>
-				<BInput
-					type="password"
-					class="text-black"
-					v-model="form.password"
-				/>
+			<BField class="col-span-2" label="Mot de passe">
+				<BInput type="password" class="text-black" v-model="form.password" />
 			</BField>
 
-			<div class="col-span-2 grid grid-cols-1 gap-4 text-center">
-				<BButton
-					variant="danger"
-					@click="submitregister"
-				>S'inscrire</BButton>
-				<BLink
-					tag="router-link"
-					to="/login"
-				>
-					J'ai déjà un compte
-				</BLink>
-		</div>
-
+			<div class="col-span-2 flex flex-col justify-center items-center">
+				<BButton class="mb-8" variant="danger" @click="submitregister">S'inscrire</BButton>
+				<BLink tag="router-link" to="/login">J'ai déjà un compte</BLink>
 			</div>
+		</div>
 	</div>
 </template>
+
 <script setup lang='ts'>
-import { computed, reactive } from 'vue'
+import { reactive } from 'vue'
 import { userHook } from '@/hooks'
 import { RoleEnum } from '@/types'
 
@@ -115,7 +73,7 @@ const form = reactive({
 	password: '',
 	firstName: '',
 	lastName: '',
- 	roles: RoleEnum.PHOTOGRAPHER
+	roles: RoleEnum.PHOTOGRAPHER
 })
 
 async function submitregister() {
