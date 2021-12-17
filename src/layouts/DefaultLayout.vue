4ed367821673e82464186e7c4933e92428f45098
malolebrin <malolebrin@gmail.com>
2021-12-17 10:26:20 +0100

    Feat register page

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,6 +2,10 @@
   <div class="flex flex-col justify-center w-full">
     <Header />
     <main class="w-full min-h-screen mx-auto">
+      <Loader
+        :isLoading="isLoading"
+        :type="LoaderTypeEnum.BOUNCE"
+      />
       <router-view />
     </main>
     <Footer />
@@ -12,14 +16,17 @@
 import { defineComponent } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useMainStore } from '@/store'
+import { LoaderTypeEnum } from '@/types'
 
 export default defineComponent({
   name: 'DefaultLayout',
   setup() {
     const mainStore = useMainStore()
     const { isLoading } = storeToRefs(mainStore)
-    return {
-      isLoading
+
+  return {
+      isLoading,
+      LoaderTypeEnum,
     }
   },
 })
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,21 +1,92 @@
 <template>
-	<div class="flex flex-col items-center h-full w-full
+	<div class="flex flex-col items-center h-full
 	bg-white">
-			<BField label="Address e-mail">
+		<div class="mb-6 mt-10">
+			<h1 class="text-black dark:text-white">Bienvenue sur</h1>
+			<SimpleLogo />
+		</div>
+
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-left">
+			<div class="flex items-center">
+				<input
+					:id="RoleEnum.PHOTOGRAPHER"
+					v-model="form.roles"
+					:value="RoleEnum.PHOTOGRAPHER"
+					:name="RoleEnum.PHOTOGRAPHER"
+					type="radio"
+					class="h-4 w-4 mr-2 border-gray-300 rounded-full text-green focus:ring-green"
+				/>
+				<label
+					:for="RoleEnum.PHOTOGRAPHER"
+					class="ml-2 block text-sm text-gray-900"
+				>
+					je suis un photographe ou agence de photographie
+				</label>
+			</div>
+			<div class="flex items-center">
+				<input
+					:id="RoleEnum.COMPANY"
+					v-model="form.roles"
+					:value="RoleEnum.COMPANY"
+					:name="RoleEnum.COMPANY"
+					type="radio"
+					class="h-4 w-4 mr-2 border-gray-300 rounded-full text-green focus:ring-green"
+				/>
+				<label
+					:for="RoleEnum.COMPANY"
+					class="ml-2 block text-sm text-gray-900"
+				>
+					je suis une enteprise ou un particulier
+				</label>
+			</div>
+
+			<BField
+				class="col-span-2"
+				label="Nom de l'entreprise"
+			>
+				<BInput
+					type="text"
+					class="text-black"
+					v-model="form.companyName"
+				/>
+			</BField>
+
+			<BField label="Prénom">
+				<BInput
+					type="text"
+					class="text-black"
+					v-model="form.firstName"
+				/>
+			</BField>
+			<BField label="Nom">
+				<BInput
+					type="text"
+					class="text-black"
+					v-model="form.lastName"
+				/>
+			</BField>
+			<BField
+				class="col-span-2"
+				label="Address e-mail"
+			>
 				<BInput
 					type="email"
 					class="text-black"
 					v-model="form.email"
 				/>
 			</BField>
-			<BField label="Mot de passe">
+			<BField
+				class="col-span-2"
+				label="Mot de passe"
+			>
 				<BInput
 					type="password"
 					class="text-black"
 					v-model="form.password"
 				/>
 			</BField>
-			<div class="grid grid-cols-1 gap-4">
+
+			<div class="col-span-2 grid grid-cols-1 gap-4 text-center">
 				<BButton
 					variant="danger"
 					@click="submitregister"
@@ -26,6 +97,7 @@
 				>
 					J'ai déjà un compte
 				</BLink>
+		</div>
 
 			</div>
 	</div>
@@ -33,17 +105,21 @@
 <script setup lang='ts'>
 import { computed, reactive } from 'vue'
 import { userHook } from '@/hooks'
+import { RoleEnum } from '@/types'
+
+const { register } = userHook()
 
 const form = reactive({
+	companyName: '',
 	email: '',
 	password: '',
 	firstName: '',
 	lastName: '',
-
+ 	roles: RoleEnum.PHOTOGRAPHER
 })
 
 async function submitregister() {
-	// await login({ email: form.email, password: form.password })
+	await register(form)
 }
 
 
