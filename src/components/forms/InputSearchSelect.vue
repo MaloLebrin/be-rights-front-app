33fdf5048259c5b63b58590d6469d00b63aa2178
malolebrin <malolebrin@gmail.com>
2021-12-21 17:14:38 +0100

    feat: input search select makes request when user stop typing

--- src/components/forms/InputSearchSelect.vue
+++ src/components/forms/InputSearchSelect.vue
@@ -1,11 +1,16 @@
 <template>
 	<div>
-		<BInput class="text-white dark:text-blue-dark" type="text" id="search" v-model="state.search" />
-		{{ state.search }}
+		<BInput
+			class="text-white dark:text-blue-dark"
+			type="text"
+			id="search"
+			v-model="state.search"
+			@keyup="searchEntity($event)"
+		/>
 	</div>
 </template>
 <script setup lang='ts'>
-import { onMounted, reactive, watch } from 'vue'
+import { reactive } from 'vue'
 import APi, { PaginatedResponse } from "@/helpers/api"
 import { useUserStore } from '@/store'
 import { EmployeeType } from '@/store/typesExported'
@@ -29,36 +34,18 @@ const state = reactive({
 	search: '',
 	data: [],
 	waitingSearch: false,
-	timeout: undefined,
+	timeout: 0,
 })
 const api = new APi(userStore.entities.current?.token!)
-const input = document.getElementById('search')
 
-onMounted(() => {
-	input?.addEventListener('keyup', (e) => {
-		if (state.timeout) {
-			clearTimeout(state.timeout)
-		}
-		state.timeout = setTimeout(() => {
-			state.waitingSearch = true
-			api.get(`${props.baseUrl}?search=${state.search}`).then((response: PaginatedResponse<EmployeeType>) => {
-				state.data = response.data
-				state.waitingSearch = false
-			})
-		}, 500)
-	})
-})
-
-watch(() => state.search, async (newValue) => {
-	console.log('search', newValue)
-	if (newValue.length > 0) {
-		clearTimeout(state.timeout)
-		setTimeout(async () => {
-			const response: PaginatedResponse<EmployeeType> = await api.get(`${props.baseUrl}?search=${newValue}&limit=999999`)
-			console.log('response', response)
+async function searchEntity(event: Event) {
+	clearTimeout(state.timeout)
+	state.timeout = setTimeout(async () => {
+		await api.get(`${props.baseUrl}?search=${state.search}`).then((response: PaginatedResponse<EmployeeType>) => {
 			state.data = response.data
-		}, 800)
-	}
-})
+		})
+	}, 500)
+}
+
 
 </script>
