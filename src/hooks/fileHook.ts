620c2a26f477443dc2fd2c722ebbe8ffdbc9f09f
malolebrin <malolebrin@gmail.com>
2022-01-14 13:28:56 +0100

    feat: add function to abstract check if entities are already in store

--- src/hooks/fileHook.ts
+++ src/hooks/fileHook.ts
@@ -1,27 +1,35 @@
 import APi from "@/helpers/api"
 import { useFileStore, useUiStore, useUserStore } from "@/store"
-import { FileType, FileTypeEnum } from "@/store/typesExported"
+import { FileType } from "@/store/typesExported"
 
 export function fileHook() {
 	const { getCurrent } = useUserStore()
+	const { getAllIds: getAllFilesIds } = useFileStore()
 	const fileStore = useFileStore()
-	const { setUIErrorToast } = useUiStore()
+	const { setUIErrorToast, IncLoading, DecLoading } = useUiStore()
 	const api = new APi(getCurrent?.token!)
 
 	async function postOne(fileForm: FormData) {
-
+		IncLoading()
 		try {
 			const res = await api.post("file", fileForm)
-			console.log(res, 'res')
 			fileStore.createOne(res as FileType)
-
 		} catch (error) {
 			console.error(error)
 			setUIErrorToast()
 		}
+		DecLoading()
+	}
+
+	function filteringFilesNotInStore(files: FileType[]) {
+		if (files.length > 0) {
+			return files.filter(file => !getAllFilesIds.includes(file.id))
+		}
+		return []
 	}
 
 	return {
 		postOne,
+		filteringFilesNotInStore,
 	}
 }
