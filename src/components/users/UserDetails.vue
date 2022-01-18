1728ad592e1dd88dbe385d500fa2fd9e9630162f
malolebrin <malolebrin@gmail.com>
2022-01-18 17:42:58 +0100

    fix: color text

--- src/components/users/UserDetails.vue
+++ src/components/users/UserDetails.vue
@@ -1,79 +1,82 @@
 <template>
-	<div v-if="user" class="w-full h-full px-6 mt-4">
-		<form class="grid grid-cols-2 gap-4">
-			<BField
-				label="Prénom"
-				labelFor="firstName"
-				class="text-blue-dark dark:text-white-break"
-			>{{ user.firstName }}</BField>
-			<BField
-				label="Nom"
-				labelFor="lastName"
-				class="text-blue-dark dark:text-white-break"
-			>{{ user.lastName }}</BField>
-			<BField
-				label="E-mail"
-				labelFor="email"
-				class="text-blue-dark dark:text-white-break"
-			>{{ user.email }}</BField>
-			<BField
-				label="Nom de l'entreprise"
-				labelFor="companyName"
-				class="text-blue-dark dark:text-white-break"
-			>{{ user.companyName }}</BField>
-			<BField
-				class="text-blue-dark dark:text-white-break"
-				label="N° Siret"
-				labelFor="siret"
-			>{{ user.siret }}</BField>
-			<BField
-				v-if="isCurrentUserAdmin"
-				label="Role"
-				labelFor="role"
-				class="text-blue-dark dark:text-white-break"
-			>{{ user.roles }}</BField>
-			<BField
-				label="Abonnement"
-				labelFor="subscription"
-				class="text-blue-dark dark:text-white-break"
-			>{{ getSuscriptionTranslation(user.subscription) }}</BField>
-		</form>
-		<div class="mt-12 text-black-light">
-			<Loader v-if="isLoading" :isLoading="isLoading" :type="LoaderTypeEnum.BOUNCE" />
-			<div class="grid grid-cols-1 mb-12 md:grid-cols-2">
-				<div
-					:class="[activeClasse(1).value, 'text-center uppercase cursor-pointer text-blue dark:text-white-break font-bold text-xl']"
-					@click="toggleActiveTab(1)"
-				>Événements</div>
-				<div
-					:class="[activeClasse(2).value, 'text-center uppercase cursor-pointer text-blue dark:text-white-break font-bold text-xl']"
-					@click="toggleActiveTab(2)"
-				>Destinaires Enregistrés</div>
-			</div>
+  <div v-if="user" class="w-full h-full px-6 mt-4">
+    <form class="grid grid-cols-2 gap-4">
+      <BField
+        label="Prénom"
+        labelFor="firstName"
+        class="text-blue-dark dark:text-white-break"
+      >{{ user.firstName }}</BField>
+      <BField
+        label="Nom"
+        labelFor="lastName"
+        class="text-blue-dark dark:text-white-break"
+      >{{ user.lastName }}</BField>
+      <BField
+        label="E-mail"
+        labelFor="email"
+        class="text-blue-dark dark:text-white-break"
+      >{{ user.email }}</BField>
+      <BField
+        label="Nom de l'entreprise"
+        labelFor="companyName"
+        class="text-blue-dark dark:text-white-break"
+      >{{ user.companyName }}</BField>
+      <BField
+        class="text-blue-dark dark:text-white-break"
+        label="N° Siret"
+        labelFor="siret"
+      >{{ user.siret }}</BField>
+      <BField
+        v-if="isCurrentUserAdmin"
+        label="Role"
+        labelFor="role"
+        class="text-blue-dark dark:text-white-break"
+      >{{ user.roles }}</BField>
+      <BField
+        label="Abonnement"
+        labelFor="subscription"
+        class="text-blue-dark dark:text-white-break"
+      >{{ getSuscriptionTranslation(user.subscription) }}</BField>
+    </form>
+    <div class="mt-12 text-black-light">
+      <Loader v-if="isLoading" :isLoading="isLoading" :type="LoaderTypeEnum.BOUNCE" />
+      <div class="grid grid-cols-1 mb-12 md:grid-cols-2">
+        <div
+          :class="[activeClasse(1).value, 'text-center uppercase cursor-pointer text-blue dark:text-white-break font-bold text-xl']"
+          @click="toggleActiveTab(1)"
+        >Événements</div>
+        <div
+          :class="[activeClasse(2).value, 'text-center uppercase cursor-pointer text-blue dark:text-white-break font-bold text-xl']"
+          @click="toggleActiveTab(2)"
+        >Destinaires Enregistrés</div>
+      </div>
 
-			<div v-if="activeTabs === 1" class="space-y-12">
-				<div class="space-y-24">
-					<EventUserItem
-						v-if="eventByUserId.length"
-						v-for="event in eventByUserId"
-						:key="event.id"
-						:event="event"
-					/>
-					<div v-else class="p-4 text-center">Aucun événement</div>
-				</div>
-			</div>
+      <div v-if="activeTabs === 1" class="space-y-12">
+        <div class="space-y-24">
+          <EventUserItem
+            v-if="eventByUserId.length"
+            v-for="event in eventByUserId"
+            :key="event.id"
+            :event="event"
+          />
+          <div v-else class="p-4 text-center text-gray-700 dark:text-gray-300">Aucun événement</div>
+        </div>
+      </div>
 
-			<div v-if="activeTabs === 2" class="space-y-8">
-				<EmployeeUserItem
-					v-if="employeeByUserId.length"
-					v-for="employee in employeeByUserId"
-					:key="employee.id"
-					:employee="employee"
-				/>
-				<div v-else class="p-4 text-center">Aucun destinataires nregistrés</div>
-			</div>
-		</div>
-	</div>
+      <div v-if="activeTabs === 2" class="space-y-8">
+        <EmployeeUserItem
+          v-if="employeeByUserId.length"
+          v-for="employee in employeeByUserId"
+          :key="employee.id"
+          :employee="employee"
+        />
+        <div
+          v-else
+          class="p-4 text-center text-gray-700 dark:text-gray-300"
+        >Aucun destinataires nregistrés</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -82,11 +85,11 @@ import { useEmployeeStore, useEventStore, useUserStore } from '@/store'
 import { LoaderTypeEnum } from '@/types'
 
 interface Props {
-	id: number
+  id: number
 }
 
 const props = withDefaults(defineProps<Props>(), {
-	id: undefined
+  id: undefined
 })
 const isLoading = ref(false)
 
@@ -100,20 +103,20 @@ const user = computed(() => getOne(props.id))
 
 
 onMounted(async () => {
-	isLoading.value = true
-	const employeeIds = user.value?.employee as number[]
-	const missingEmployeeIds = employeeIds.filter(id => !employeeStore.getOne(id))
+  isLoading.value = true
+  const employeeIds = user.value?.employee as number[]
+  const missingEmployeeIds = employeeIds.filter(id => !employeeStore.getOne(id))
 
-	if (missingEmployeeIds.length > 0) {
-		await fetchAllEmployeeByUserId(user.value.id)
-	}
-	const eventIds = user.value?.events as number[]
-	const missingEventIds = eventIds.filter(id => !eventStore.getOne(id))
+  if (missingEmployeeIds.length > 0) {
+    await fetchAllEmployeeByUserId(user.value.id)
+  }
+  const eventIds = user.value?.events as number[]
+  const missingEventIds = eventIds.filter(id => !eventStore.getOne(id))
 
-	if (missingEventIds.length > 0) {
-		await eventStore.fetchAllByUserId(user.value.id)
-	}
-	isLoading.value = false
+  if (missingEventIds.length > 0) {
+    await eventStore.fetchAllByUserId(user.value.id)
+  }
+  isLoading.value = false
 })
 
 const eventByUserId = computed(() => eventStore.getMany(user.value.events as number[]))
@@ -125,6 +128,6 @@ const activeTabs = ref(1)
 const activeClasse = (tab: number) => computed(() => activeTabs.value === tab ? 'border-b-4 border-green-300' : '')
 
 function toggleActiveTab(tab: number) {
-	activeTabs.value = tab
+  activeTabs.value = tab
 }
 </script>
