bbb59e806f4eae8a5d7e3aab5a1282feff5a6ee3
malolebrin <malolebrin@gmail.com>
2022-03-30 17:39:55 +0200

    feat: 404 page minimum implemented

---
+++ src/pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center justify-center w-full min-h-screen space-y-4">
+    <h2 class="text-2xl font-bold dark:text-white">Oops Vous n'êtes pas sur la bonne page</h2>
+
+    <BaseButton href="/">retourner à l'accueil</BaseButton>
+  </div>
+</template>
