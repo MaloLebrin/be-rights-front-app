e4165d470b805f3fa96c01d1b0568b84da917f32
malolebrin <malolebrin@gmail.com>
2022-04-04 21:51:34 +0200

    feat: event status tag

---
+++ src/components/events/EventStatusTag.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative flex mt-px sm:col-start-3">
+    <div
+      :class="`flex flex-col p-2 overflow-y-auto text-xs leading-5 rounded-lg group inset-1 bg-${getStatusColor()}-50 hover:bg-${getStatusColor()}-100`"
+    >
+      <p
+        :class="`text-${getStatusColor()}-500 group-hover:text-${getStatusColor()}-700 font-semibold uppercase`"
+      >{{ getEventStatusTranslation(status) }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { EventStatusEnum } from '@/types/typesExported'
+
+interface Props {
+  status: EventStatusEnum
+}
+
+const props = defineProps<Props>()
+
+const { getEventStatusTranslation } = eventHook()
+
+function getStatusColor() {
+  switch (props.status) {
+    case EventStatusEnum.COMPLETED:
+      return 'sky'
+    case EventStatusEnum.PENDING:
+      return 'orange'
+    case EventStatusEnum.CLOSED:
+      return 'gray'
+    case EventStatusEnum.CREATE:
+      return 'green'
+    default:
+      return 'gray'
+  }
+}
+</script>
