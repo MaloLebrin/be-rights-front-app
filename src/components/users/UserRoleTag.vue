e20147383b4d35caf157d987f8bd56029279721a
malolebrin <malolebrin@gmail.com>
2022-04-04 21:30:28 +0200

    feat: Tags for role user and subscription

---
+++ src/components/users/UserRoleTag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="relative flex mt-px sm:col-start-3">
+    <div
+      :class="`flex flex-col p-2 overflow-y-auto text-xs leading-5 rounded-lg group inset-1 bg-${getRoleColor()}-50 hover:bg-${getRoleColor()}-100`"
+    >
+      <p
+        :class="`text-${getRoleColor()}-500 group-hover:text-${getRoleColor()}-700 font-semibold`"
+      >{{ getRoleTranslation(role) }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RoleEnum } from '@/types'
+
+interface Props {
+  role: RoleEnum
+}
+
+const props = defineProps<Props>()
+
+const { getRoleTranslation } = userHook()
+
+function getRoleColor() {
+  switch (props.role) {
+    case RoleEnum.ADMIN:
+      return 'sky'
+    case RoleEnum.USER:
+      return 'orange'
+    case RoleEnum.EMPLOYEE:
+      return 'gray'
+    case RoleEnum.SUPER_USER:
+      return 'green'
+    case RoleEnum.COMPANY:
+      return 'red'
+    case RoleEnum.PHOTOGRAPHER:
+      return 'purple'
+    case RoleEnum.CUSTOMER:
+      return 'lime'
+    default:
+      return 'gray'
+  }
+}
+</script>
