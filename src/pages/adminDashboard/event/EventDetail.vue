1fee2276cb0c99910ed8a02aa9403f5b508c1a65
malolebrin <malolebrin@gmail.com>
2022-04-04 23:20:51 +0200

    feat: init branche and page

---
+++ src/pages/adminDashboard/event/EventDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="container relative min-h-screen py-6 pr-8 mx-auto text-left transition-all duration-500 ease-in-out transform"
+  >
+    <HeaderList>
+      <template #title>
+        <CalendarIconOutline class="h-8 p-1 mr-4 rounded-lg dark:bg-red" />
+        {{ event.name }}
+      </template>
+    </HeaderList>
+    <div class="py-4 mt-24 rounded-lg shadow-lg"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const eventStore = useEventStore()
+const { params } = useRoute()
+
+const eventId = computed(() => parseInt(params.eventId as string))
+const event = computed(() => eventStore.getOne(eventId.value))
+</script>
