241b9e83b26532b1dee85f7fa68bd9be9067c952
malolebrin <malolebrin@gmail.com>
2022-04-12 09:56:18 +0200

    feat: add routing to links in footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,30 +1,26 @@
 <template>
-  <footer class="shadow-3xl dark:bg-blue-dark_bold DarkModeAnimation w-full z-50">
+  <footer class="z-50 w-full shadow-3xl dark:bg-blue-dark_bold DarkModeAnimation">
     <div class="grid grid-cols-1 md:grid-cols-2">
       <div
-        class="w-full h-full flex flex-col mb-6 text-black justify-between mx-auto items-center py-6 dark:text-white bg-white dark:bg-blue-dark_bold"
-      >
+        class="flex flex-col items-center justify-between w-full h-full py-6 mx-auto mb-6 text-black bg-white dark:text-white dark:bg-blue-dark_bold">
         <SimpleLogo class="mb-10" />
         <DarkModeToggle />
         {{ `© Be Right ${new Date().getFullYear()}` }}
       </div>
       <div class="flex-1 py-6 bg-gray-800 dark:bg-transparent">
-        <p class="uppercase lg:mt-4 text-gray-200 md:mb-6">Menu</p>
-        <ul class="list-reset mb-6">
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a
-              href="#"
-              class="no-underline hover:underline text-white hover:text-red-light"
-            >Se connecter</a>
+        <p class="text-gray-200 uppercase lg:mt-4 md:mb-6">Menu</p>
+        <ul class="mb-6 list-reset">
+          <li class="inline-block mt-2 mr-2 md:block md:mr-0">
+            <router-link :to="{ name: 'login' }" class="text-white no-underline hover:underline hover:text-red-light">Se
+              connecter</router-link>
           </li>
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a
-              href="#"
-              class="no-underline hover:underline text-white hover:text-red-light"
-            >s'inscrire</a>
+          <li class="inline-block mt-2 mr-2 md:block md:mr-0">
+            <router-link :to="{ name: 'register' }" class="text-white no-underline hover:underline hover:text-red-light">
+              s'inscrire</router-link>
           </li>
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="#" class="no-underline hover:underline text-white hover:text-red-light">Accueil</a>
+          <li class="inline-block mt-2 mr-2 md:block md:mr-0">
+            <router-link :to="{ name: 'home' }" class="text-white no-underline hover:underline hover:text-red-light">
+              Accueil</router-link>
           </li>
         </ul>
         <p class="text-xs text-gray-500">designed by digital campus students</p>
