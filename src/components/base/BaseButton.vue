f43cf07ebf9dd60ff8a6ecd9d409dd073ce199c0
malolebrin <malolebrin@gmail.com>
2022-04-12 18:59:43 +0200

    fix: basebutton variation color in darkmode

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,22 +1,14 @@
 <template>
-  <RouterLink
-    v-if="href"
-    v-bind="$attrs"
-    :disabled="disabled"
-    :aria-disabled="disabled"
-    :to="href"
-    :class="[
-      'flex justify-center px-4 py-2 sm:text-sm md:text-base font-medium border border-transparent rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 transform transition hover:-translate-y-1 hover:scale-105 duration-300 ease-in-out',
-      colorClasses,
-      { 'inline-flex flex-row items-center space-x-2': $slots.icon },
-      { 'justify-start': $slots.icon && $slots.default },
-      { 'justify-center': $slots.icon && !$slots.default },
-      { 'cursor-not-allowed opacity-70': disabled },
-      'font-link',
-      $attrs['class'] || '',
-    ]"
-    @click="onClick($event)"
-  >
+  <RouterLink v-if="href" v-bind="$attrs" :disabled="disabled" :aria-disabled="disabled" :to="href" :class="[
+    'flex justify-center px-4 py-2 sm:text-sm md:text-base font-medium border border-transparent rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 transform transition hover:-translate-y-1 hover:scale-105 duration-300 ease-in-out',
+    colorClasses,
+    { 'inline-flex flex-row items-center space-x-2': $slots.icon },
+    { 'justify-start': $slots.icon && $slots.default },
+    { 'justify-center': $slots.icon && !$slots.default },
+    { 'cursor-not-allowed opacity-70': disabled },
+    'font-link',
+    $attrs['class'] || '',
+  ]" @click="onClick($event)">
     <span v-if="$slots.icon" class="w-6 h-6">
       <slot name="icon" />
     </span>
@@ -25,24 +17,15 @@
       <div class="w-6 h-6 ml-4 border-t-2 border-b-2 border-white rounded-full animate-spin" />
     </template>
   </RouterLink>
-  <component
-    :is="tag"
-    v-else
-    v-bind="$attrs"
-    :type="type"
-    :disabled="disabled"
-    :aria-disabled="disabled"
-    :class="[
-      'flex items-center justify-center px-4 py-2 sm:text-sm md:text-base font-medium border border-transparent rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 transform transition hover:-translate-y-1 hover:scale-105 duration-300 ease-in-out',
-      colorClasses,
-      { 'inline-flex flex-row items-center space-x-2': $slots.icon },
-      { 'justify-start': $slots.icon && $slots.default },
-      { 'justify-center': $slots.icon && !$slots.default },
-      { 'cursor-not-allowed opacity-70': disabled },
-      $attrs['class'] || '',
-    ]"
-    @click="onClick($event)"
-  >
+  <component :is="tag" v-else v-bind="$attrs" :type="type" :disabled="disabled" :aria-disabled="disabled" :class="[
+    'flex items-center justify-center px-4 py-2 sm:text-sm md:text-base font-medium border border-transparent rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 transform transition hover:-translate-y-1 hover:scale-105 duration-300 ease-in-out',
+    colorClasses,
+    { 'inline-flex flex-row items-center space-x-2': $slots.icon },
+    { 'justify-start': $slots.icon && $slots.default },
+    { 'justify-center': $slots.icon && !$slots.default },
+    { 'cursor-not-allowed opacity-70': disabled },
+    $attrs['class'] || '',
+  ]" @click="onClick($event)">
     <div v-if="$slots.icon" class="w-6 h-6">
       <slot name="icon" />
     </div>
@@ -92,7 +75,7 @@ const colorClasses = computed(() => {
     backgroundClass = 'bg-blue'
   }
 
-  if (['white'].includes(props.color) || mainStore.isDarkTheme) {
+  if (props.color === 'white' || (mainStore.isDarkTheme && !['red', 'green'].includes(props.color))) {
     textColorClass = 'text-blue'
     backgroundClass = 'bg-white'
   }
