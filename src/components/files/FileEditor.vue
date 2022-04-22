620da3f8b63324508cf66d0d42d60ea9b799f951
malolebrin <malolebrin@gmail.com>
2022-04-22 10:13:38 +0200

    WIP: testing editor tiptap

---
+++ src/components/files/FileEditor.vue
@@ -0,0 +1,17 @@
+<template>
+<div v-if="editor">
+  <editor-content :editor="editor" />
+</div>
+</template>
+
+<script setup lang="ts">
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [
+    StarterKit,
+  ],
+})
+</script>
