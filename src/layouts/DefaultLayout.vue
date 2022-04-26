44330f5d83422542769d9776c8a4b196d46571c3
malolebrin <malolebrin@gmail.com>
2022-04-26 18:13:18 +0200

    fix: col organisation on default layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="w-full min-h-screen">
   <Header />
-  <main class="flex w-full min-h-screen md:mx-auto md:container-lg">
+  <main class="flex flex-col w-full min-h-screen md:mx-auto md:container-lg">
     <router-view />
   </main>
   <Footer />
