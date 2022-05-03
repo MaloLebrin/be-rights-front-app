a1467e4af275e847f29affbcbf7bda0d37a5b834
malolebrin <malolebrin@gmail.com>
2022-05-03 15:12:26 +0200

    fix: somes styles like icon isloading button

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -23,9 +23,33 @@
   >
     <slot name="icon" />
   </span>
-  <slot />
-  <template v-if="isLoading">
-    <div class="w-6 h-6 ml-4 border-t-2 border-b-2 border-white rounded-full animate-spin" />
+  <div
+    v-if="isLoading"
+    class="flex items-center justify-center w-full h-full"
+  >
+    <svg
+      :class="`text-white-400 animate-spin h-6 w-6`"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      />
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  </div>
+  <template v-else>
+    <slot />
   </template>
 </RouterLink>
 <component
@@ -52,10 +76,37 @@
   >
     <slot name="icon" />
   </div>
-  <div class="flex items-center justify-center">
-    <slot />
-    <template v-if="isLoading">
-      <div class="w-6 h-6 ml-4 border-t-2 border-b-2 border-white rounded-full animate-spin" />
+  <div
+
+    class="flex items-center justify-center"
+  >
+    <div
+      v-if="isLoading"
+      class="flex items-center justify-center w-full h-full"
+    >
+      <svg
+        :class="`text-white-600 animate-spin h-6 w-6`"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        />
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        />
+      </svg>
+    </div>
+    <template v-else>
+      <slot />
     </template>
   </div>
 </component>
--- src/pages/adminDashboard/event/EventId.vue
+++ src/pages/adminDashboard/event/EventId.vue
@@ -2,12 +2,10 @@
 <div
   class="container min-h-screen px-8 py-6 text-left transition-all duration-500 ease-in-out transform md:px-20 lg:px-32"
 >
-  <div class="py-4 mt-24 rounded-lg shadow-lg">
-    <EventForm
-      :event-id="eventId"
-      :mode="ModalModeEnum.EDIT"
-    />
-  </div>
+  <EventForm
+    :event-id="eventId"
+    :mode="ModalModeEnum.EDIT"
+  />
 </div>
 </template>
 
