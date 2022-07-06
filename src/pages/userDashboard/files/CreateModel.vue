1bb78d1e629ba0a25c594ee33d3964c002c31e2b
MaloLebrin <malolebrin@gmail.com>
2022-07-06 15:12:12 +0200

    rebase with main

--- src/pages/userDashboard/files/CreateModel.vue
+++ src/pages/userDashboard/files/CreateModel.vue
@@ -1,7 +1,6 @@
 <template>
 <div class="min-h-screen px-8 py-6 text-left transition-all duration-500 ease-in-out transform md:px-20 lg:px-32">
   <div class="mt-4">
-    <!-- <FileForm :mode="ModalModeEnum.CREATE" /> -->
     <FileEditor />
   </div>
 </div>
