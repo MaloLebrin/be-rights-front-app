6b5c16a8130bd05dda18be5d26ae94554c4b31df
MaloLebrin <malolebrin@gmail.com>
2022-08-31 14:57:17 +0200

    fix: redirection and guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { useCookies } from 'vue3-cookies'
 import { mainRoutes } from './mainRoutes'
 import { userRoutes } from './userRoutes'
 import { adminRoutes } from './adminRoutes'
@@ -18,44 +17,26 @@ const router = createRouter({
 
 router.beforeResolve((to, _from, next) => {
   const userStore = useUserStore()
-  const { cookies } = useCookies()
-  // const { loginWithToken } = authHook()
 
   const { isAuth, isAdmin } = to.meta
-  let token: string | null = null
-
   if (!isAuth) {
     return next()
   }
-
-  if (isAuth && !userStore.getCurrent) {
-    token = cookies.get('token')
-    if (token) {
-      // await loginWithToken(token)
-      if (userStore.getCurrent) {
-        if (!isAdmin) {
-          return next()
-        }
-        if (isAdmin && userStore.isCurrentUserAdmin) {
-          return next()
-        }
-        return next(to)
+  console.log(userStore.getCurrentUserId, '<==== userStore.getCurrentUserId')
+  if (isAuth) {
+    if (userStore.getCurrent) {
+      if (!isAdmin) {
+        return next()
       }
-    } else {
-      return next({
-        name: 'login',
-      })
-    }
-  }
-  if (isAuth && userStore.getCurrent) {
-    if (!isAdmin) {
+      if (isAdmin && userStore.isCurrentUserAdmin) {
+        return next()
+      }
       return next()
     }
-    if (isAdmin && userStore.isCurrentUserAdmin) {
-      return next()
-    }
-    return next(to)
   }
+  return next({
+    name: 'login',
+  })
 })
 
 declare module 'vue-router' {
