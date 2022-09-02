3deab1e12782d72f366f47060dfac8a705c8b2fb
MaloLebrin <malolebrin@gmail.com>
2022-09-02 11:19:59 +0200

    refacto: component to display creator user of event or employee

--- src/components/employees/EmployeeDetails.vue
+++ src/components/employees/EmployeeDetails.vue
@@ -6,7 +6,7 @@
       <div>
         <img
           class="object-cover w-full h-56"
-          :src="'https://images.unsplash.com/photo-1460132011327-1bcd44f7ae20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9ncmFwaGVyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'"
+          src="https://images.unsplash.com/photo-1460132011327-1bcd44f7ae20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9ncmFwaGVyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
           alt="Photo de couverture du destinataire"
         >
       </div>
@@ -147,45 +147,11 @@
       </dl>
     </div>
 
-    <div
+    <EmployeeCreator
       v-if="userStore.isCurrentUserAdmin && employeeCreator"
-      class="max-w-5xl px-4 pb-12 mx-auto mt-8 sm:px-6 lg:px-8"
-    >
-      <h2 class="text-sm font-medium text-gray-500">
-        Créé par
-      </h2>
-      <div class="grid grid-cols-1 gap-4 mt-1 sm:grid-cols-2">
-        <div class="relative flex items-center px-6 py-5 space-x-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500">
-          <div
-            v-if="employeeCreator.profilePicture"
-            class="flex-shrink-0"
-          >
-            <img
-              class="w-10 h-10 rounded-full"
-              :src="employeeCreator.profilePicture"
-              alt="Photo de profile du créateur du destinataire"
-            >
-          </div>
-          <div class="flex-1 min-w-0">
-            <RouterLink
-              :to="{ name: 'admin.users.show', params: { userId: employeeCreator.id } }"
-              class="focus:outline-none"
-            >
-              <span
-                class="absolute inset-0"
-                aria-hidden="true"
-              />
-              <p class="text-sm font-medium text-gray-900 underline cursor-pointer">
-                {{ getUserfullName(employeeCreator) }}
-              </p>
-              <p class="text-sm text-gray-500 truncate">
-                {{ employeeCreator.companyName }}
-              </p>
-            </RouterLink>
-          </div>
-        </div>
-      </div>
-    </div>
+      class="mt-4"
+      :employee-creator="employeeCreator"
+    />
   </article>
 </main>
 </template>
