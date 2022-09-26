fd84b6e1700696d8e2337b9ad30f865ef61b8c0e
MaloLebrin <malolebrin@gmail.com>
2022-09-26 18:05:33 +0200

    refacto: use create action instead of custom action created at the beginning of project

--- src/store/employee/employeeStore.ts
+++ src/store/employee/employeeStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { createGetters } from '@malolebrin/pinia-entity-store'
+import { createActions, createGetters } from '@malolebrin/pinia-entity-store'
 import { defaultEmployeeState, employeState } from './state'
 import type { EmployeeType } from './types'
 import { EntitiesEnum } from '@/types/globals'
@@ -22,50 +22,10 @@ export const useEmployeeStore = defineStore(EntitiesEnum.EMPLOYEES, {
   },
   actions: {
     // actions common to all entities
-    createOne(payload: EmployeeType) {
-      this.entities.byId[payload.id] = payload
-      this.entities.allIds.push(payload.id)
-    },
-    createMany(payload: EmployeeType[]) {
-      payload.forEach(entity => this.createOne(entity))
-    },
-    setCurrent(payload: EmployeeType) {
-      this.entities.current = payload
-    },
-    removeCurrent() {
-      this.entities.current = null
-    },
-    updateOne(id: number, payload: EmployeeType): void {
-      if (this.isAlreadyInStore(id)) {
-        const entity = this.entities.byId[id]
-        this.entities.byId[id] = {
-          ...entity,
-          ...payload,
-        }
-      } else {
-        this.createOne(payload)
-      }
-    },
-    updateMany(payload: EmployeeType[]): void {
-      payload.forEach(entity => this.updateOne(entity.id, entity))
-    },
-    deleteOne(id: number) {
-      delete this.entities.byId[id]
-      this.entities.allIds = this.entities.allIds.filter(entityId => entityId !== id)
-    },
-    deleteMany(ids: number[]) {
-      ids.forEach(id => this.deleteOne(id))
-    },
+    ...createActions<EmployeeType>(employeState),
+
     resetState() {
       this.$state = defaultEmployeeState()
-    },
-    setActive(id: number) {
-      if (!this.isAlreadyActive(id)) {
-        this.entities.active.push(id)
-      }
-    },
-    resetActive() {
-      this.entities.active = []
     },
   },
 })
