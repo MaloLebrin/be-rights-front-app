ecc880eaa3f9b620e3b177b40fee38edf9b19937
MaloLebrin <malolebrin@gmail.com>
2022-10-05 19:25:07 +0200

    feat: add dashboard item component

---
+++ components/dashboard/DashboardItem.vue
@@ -0,0 +1,56 @@
+<template>
+<div
+  :id="`accordion${index}`"
+  class="flex items-center lg:w-full"
+>
+  <BaseAccordion
+    class="flex flex-col items-center w-3/4 max-w-3xl bg-white rounded-full shadow-2xl dark:bg-blue-dark_bold hover:-translate-y-2 animate-fade-in-down"
+  >
+    <template #title>
+      <slot name="title" />
+    </template>
+
+    <slot />
+  </BaseAccordion>
+
+  <div
+    v-if="hasSlotButtons"
+    class="flex flex-col w-1/5 px-4 py-2 ml-4 text-xs transition-all duration-500 ease-in-out rounded-xl"
+    :class="extraButtonOpen === index ? 'opacity-1' : 'opacity-0'"
+  >
+    <slot name="extraButton" />
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  index: {
+    type: Number as PropType<number>,
+    required: true,
+  },
+})
+
+const extraButtonOpen = ref<null | number>(null)
+
+const slots = useSlots()
+const hasSlotButtons = computed(() => slots.extraButton)
+
+onMounted(() => {
+  const item = document.getElementById(`accordion${props.index}`)
+  item?.addEventListener('mouseover', () => {
+    extraButtonOpen.value = props.index
+  })
+  item?.addEventListener('mouseout', () => {
+    extraButtonOpen.value = null
+  })
+})
+
+onBeforeUnmount(() => {
+  const item = document.getElementById(`accordion${props.index}`)
+  item?.removeEventListener('mouseover', () => extraButtonOpen.value = null)
+  item?.removeEventListener('mouseout', () => extraButtonOpen.value = null)
+})
+</script>
