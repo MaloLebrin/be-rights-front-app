f819e470ae0ce61186a5f9685a83da364ea3f277
malolebrin <malolebrin@gmail.com>
2022-12-28 18:26:39 +0100

    ♻️ Clean up code for PR cypress on github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4173',
+    baseUrl: process.env.NODE_ENV === 'prod' ? 'http://localhost:4173' : 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
